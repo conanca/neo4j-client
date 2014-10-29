a4b4d278bf14a02fe1b47ddc10186e435cc20039
conanca <imconan@gmail.com>
2014-10-29 14:42:48 +0800

    修改节点相关操作

--- src/main/java/com/xinhuanet/commons/neo4j/Neo4jClient.java
+++ src/main/java/com/xinhuanet/commons/neo4j/Neo4jClient.java
@@ -2,9 +2,12 @@ package com.xinhuanet.commons.neo4j;
 
 import java.util.Map;
 
-import org.springframework.stereotype.Service;
+import javax.ws.rs.core.Response;
 
-import com.xinhuanet.entity.ResData;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import com.alibaba.fastjson.JSON;
 
 @Service("neo4jClient")
 public class Neo4jClient extends RestfulClient {
@@ -17,29 +20,64 @@ public class Neo4jClient extends RestfulClient {
 		super(url, httpAuthUserName, httpAuthPassword);
 	}
 
-	public Boolean createNode(Integer userId, Integer typeCode, Map<String, String> param) {
-
-		return true;
+	public Boolean createNode(Map<String, String> para) {
+		Response resp = this.post(Lang.list("node"), para);
+		if (resp.getStatus() == 201) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
-	public ResData getNode(Integer userid) {
-		return null;
+	public Map getNode(Long nodeId) {
+		Response resp = this.get(Lang.list("node", String.valueOf(nodeId)));
+		if (resp.getStatus() == 200) {
+			String jsonResp = resp.readEntity(String.class);
+			if (!StringUtils.isEmpty(jsonResp)) {
+				Map m = JSON.parseObject(jsonResp, Map.class);
+				return m;
+			} else {
+				return null;
+			}
+		} else {
+			return null;
+		}
 	}
 
-	public Boolean updateNode(Integer userId, Map<String, String> param) {
-		return true;
+	public Boolean addLabels(Long nodeId, String... labels) {
+		Response resp = this.post(Lang.list("node", String.valueOf(nodeId)), JSON.toJSONString(labels));
+		if (resp.getStatus() == 204) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
-	public Boolean changeNodeType(Integer userId, Integer oldTypeCode, Integer newTypeCode) {
-		return true;
+	public Boolean deleteLabel(Long nodeId, String label) {
+		Response resp = this.delete(Lang.list("node", String.valueOf(nodeId), "labels", JSON.toJSONString(label)));
+		if (resp.getStatus() == 204) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
-	public Boolean deleteNodeRelation(Integer userId) {
-		return true;
+	public Boolean updateNode(Long nodeId, Map<String, String> para) {
+		Response resp = this.put(Lang.list("node", String.valueOf(nodeId), "properties"), para);
+		if (resp.getStatus() == 204) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
-	public Boolean deleteNode(Integer userId) {
-		return true;
+	public Boolean deleteNode(Long nodeId) {
+		Response resp = this.delete(Lang.list("node", String.valueOf(nodeId)));
+		if (resp.getStatus() == 204) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	public Boolean createRelation(Integer startUserId, Integer endUserId, String relType, Map<String, String> param) {
