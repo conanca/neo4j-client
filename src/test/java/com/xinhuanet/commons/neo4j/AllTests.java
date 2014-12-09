4b03fbcc3deaceaaacf31fbd18e391b2f528eede
conanca <imconan@gmail.com>
2014-12-09 11:24:03 +0800

    增加Suite

---
+++ src/test/java/com/xinhuanet/commons/neo4j/AllTests.java
@@ -0,0 +1,11 @@
+package com.xinhuanet.commons.neo4j;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ RestfulClientTest.class, Neo4jClientPoolTest.class, Neo4jClientTest.class, })
+public class AllTests {
+
+}
